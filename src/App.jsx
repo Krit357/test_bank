254ff90432c29c6fbdc7916640c7a77b929cf055
Chris357 <Chris@eup.co.th>
2025-06-09 11:31:24 +0700

    add Fixedhomepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-// import FixedHomePage from "./components/FixedHomePage";
-// import StandardPage from "./components/StandardPage";
+import FixedHomePage from "./components/FixedHomePage";
+import StandardPage from "./components/StandardPage";
 
 function App() {
   return (
     <div>
-      <p>test</p>
+      <FixedHomePage />
     </div>
   );
 }
